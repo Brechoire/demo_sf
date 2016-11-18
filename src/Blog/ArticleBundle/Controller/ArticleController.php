fd382e9450d4e1990a7a7677b59a8cf55394e0cb
brechoire <brechoire.j@gmail.com>
2016-11-18 11:55:36 +0100

    first commit

---
+++ src/Blog/ArticleBundle/Controller/ArticleController.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AnCat
+ * Date: 17/11/2016
+ * Time: 19:34
+ */
+
+namespace Blog\ArticleBundle\Controller;
+
+use Blog\ArticleBundle\Entity\Article;
+use Blog\ArticleBundle\Form\ArticleType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * @Route("/", name="home-page")
+     * @Template("default/index.html.twig")
+     */
+    public function indexAction()
+    {
+
+        $repository = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('BlogArticleBundle:Article');
+
+        $count = $repository->countArticle();
+        $showArticle = $repository->showArticles();
+
+        return array(
+            'countarticle' => $count,
+            'showarticle' => $showArticle
+        );
+
+    }
+
+    /**
+     * @Route("/add", name="add")
+     * @Template("default/add.html.twig")
+     */
+    public function addArticleAction(Request $request)
+    {
+
+        $form = $this->createForm(ArticleType::class, new Article());
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+//            dump($form->getData());
+//            die;
+
+            $article = $form->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+            return $this->redirectToRoute('home-page');
+        }
+
+        return array('form' => $form->createView());
+    }
+}
