93c4900cab1c778c9b2783b50af0617e85686de9
brechoire <brechoire.j@gmail.com>
2016-11-22 00:07:28 +0100

    create User

--- src/Blog/ArticleBundle/Controller/ArticleController.php
+++ src/Blog/ArticleBundle/Controller/ArticleController.php
@@ -9,7 +9,9 @@
 namespace Blog\ArticleBundle\Controller;
 
 use Blog\ArticleBundle\Entity\Article;
+use Blog\ArticleBundle\Entity\User;
 use Blog\ArticleBundle\Form\ArticleType;
+use Blog\ArticleBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -121,5 +123,38 @@ class ArticleController extends Controller
 
     }
 
+    /**
+     * @Route("/register", name="register")
+     * @Template("default/register.html.twig")
+     */
+    public function registerAction(Request $request)
+    {
+// 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $this->get('security.password_encoder')
+                ->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('home-page');
+        }
+
+        return array('form' => $form->createView());
+
+    }
 
 }
